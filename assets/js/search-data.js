29e35c0fea908f089ad1ee6ff3f7c9a64b6d1339
jan-kula <jan.kula1999@gmail.com>
2026-08-13 16:45:43 +0000

    Deploying to gh-pages from @ jan-kula/jan-kula.github.io@c7ebc529afdc3e7fd729878d9433529a59b3baef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,8 +17,8 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "Here are my projects, take a look!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
