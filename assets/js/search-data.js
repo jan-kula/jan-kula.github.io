2c912d6cc2bdebbde0bab38cc164a3901a6557d6
jan-kula <jan.kula1999@gmail.com>
2026-08-20 16:37:42 +0000

    Deploying to gh-pages from @ jan-kula/jan-kula.github.io@ada51ea3533cffa1db9575be4ed2bd01ed9edaa9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here is some information that is relevant to any prospect employer and some of my interests. You can also download my cv here!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
